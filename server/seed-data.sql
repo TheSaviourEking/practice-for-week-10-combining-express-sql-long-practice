c587901b1d5078274b2e256d8498efc31402d5b6
TheSaviourEking <saviour.eking@outlook.com>
2024-03-04 10:36:26 +0100

    chore: implement phase 1B: INSERT records

--- server/seed-data.sql
+++ server/seed-data.sql
@@ -13,26 +13,26 @@ CREATE TABLE IF NOT EXISTS
 
 -- BASIC PHASE 1B - INSERT seed data
 -- Your code here
--- INSERT INTO
---   trees (
---     tree,
---     location,
---     height_ft,
---     ground_circumference_ft
---   )
--- VALUES
---   (
---     'General Sherman',
---     'Sequoia National Park',
---     274.9,
---     102.6
---   ),
---   (
---     'General Grant',
---     'Kings Canyon National Park',
---     268.1,
---     107.5
---   ),
---   ('President', 'Sequoia National Park', 240.9, 93.0),
---   ('Lincoln', 'Squoia National Park', 255.8, 98.3),
---   ('Stagg', 'Private Land', 243.0, 109.0);
+INSERT INTO
+  trees (
+    tree,
+    location,
+    height_ft,
+    ground_circumference_ft
+  )
+VALUES
+  (
+    'General Sherman',
+    'Sequoia National Park',
+    274.9,
+    102.6
+  ),
+  (
+    'General Grant',
+    'Kings Canyon National Park',
+    268.1,
+    107.5
+  ),
+  ('President', 'Sequoia National Park', 240.9, 93.0),
+  ('Lincoln', 'Squoia National Park', 255.8, 98.3),
+  ('Stagg', 'Private Land', 243.0, 109.0);
